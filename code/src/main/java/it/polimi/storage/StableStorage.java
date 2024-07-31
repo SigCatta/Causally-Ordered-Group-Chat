c2f5e328530b4586e81e83d39ece4df63bbefb60
Luca Cattani <luca.cattani@mail.polimi.it>
2024-07-31 17:26:29 +0200

    added a few comments and suggestion to fix the StringBuilder problem

--- code/src/main/java/it/polimi/storage/StableStorage.java
+++ code/src/main/java/it/polimi/storage/StableStorage.java
@@ -1,9 +1,5 @@
 package it.polimi.storage;
 
-import java.lang.StringBuilder;
-import java.util.Arrays;
-import java.util.List;
-
 public class StableStorage {
     private final StorageWriter sw;
     private final StorageReader sr;
@@ -13,7 +9,12 @@ public class StableStorage {
         this.sr = new StorageReader();
     }
 
+    // Creates and initializes all the files needed for a chat room
     public void initNewRoom(String roomId, int size) {
+        if (size < 2) {
+            System.out.println("Go get some friends...");
+            throw new IllegalArgumentException("Room size must be at least 2");
+        }
         // Create necessary files
         sw.createFile(roomId + "/messages.txt");
         sw.createFile(roomId + "/vector_clocks.txt");
@@ -24,9 +25,9 @@ public class StableStorage {
 
         StringBuilder sb = new StringBuilder() // Build the first vector clock: (0,0,...,0,0)
                 .append("[");
-
-               // .repeat("0,", size - 1)
-        for(int i = 0; i < size - 1; i++) {
+        // .repeat("0,", size - 1)
+        // .append("0,".repeat(size - 1));
+        for (int i = 0; i < size - 1; i++) {
             sb.append("0,");
         }
         sb.append("0]\n");
@@ -34,11 +35,15 @@ public class StableStorage {
         sw.append(roomId + "/last_vc.txt", sb.toString());
     }
 
+    // Returns the current vector clock
+    public VectorClock getCurrentVectorClock(String roomId) {
+        return sr.getCurrentVectorClock(roomId);
+    }
+
+    // Saves a message to stable storage, along with the vector clock
     public void persistMessage(String roomId, String message, VectorClock vc) {
 
         sw.append(roomId + "/messages.txt", message.replace("\n", " "));
     }
-
-
 
 }
--- code/src/main/java/it/polimi/storage/StorageReader.java
+++ code/src/main/java/it/polimi/storage/StorageReader.java
@@ -7,14 +7,17 @@ import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 
-public class StorageReader {
+class StorageReader {
     // Initialize the PATH variable, this is the directory where all files will be stored
     private final String PATH = String.valueOf(Paths.get(System.getProperty("user.home"), "chat_ss"));
 
-    public VectorClock getLatestVectorClock(String roomId) {
+    // Returns the last available vector clock (for delivered messages)
+    public VectorClock getCurrentVectorClock(String roomId) {
+        // Get the string containing the current vector clock
         Path location = Paths.get(PATH, roomId, "last_vc.txt");
         String vc = catFile(location).getFirst();
 
+        // Convert the string to a list of integers
         List<Integer> vector =
                 Arrays.stream(vc.substring(1, vc.length() - 1) // Remove brackets
                                 .replace(" ", "")
@@ -24,6 +27,7 @@ public class StorageReader {
         return new VectorClock(vector);
     }
 
+    // Reads all lines from a file
     private List<String> catFile(Path path) {
         try {
             return Files.readAllLines(path);
